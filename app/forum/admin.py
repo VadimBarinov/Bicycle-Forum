310c1996b7aa41baacf0150390c56d2df9186fba
VadimBarinov <barinovvadim87@gmail.com>
2025-11-19 22:24:59 +0400

    admin panel

--- app/forum/admin.py
+++ app/forum/admin.py
@@ -14,6 +14,10 @@ class SectionAdmin(admin.ModelAdmin):
         "title",
         "created_at",
     )
+    list_display_links = (
+        "id",
+        "title",
+    )
 
 
 @admin.register(Theme)
@@ -26,16 +30,41 @@ class ThemeAdmin(admin.ModelAdmin):
         "messages_count",
         "created_at",
     )
-
+    list_display_links = (
+        "id",
+        "title",
+    )
+    fields = [
+        "title",
+        "section",
+    ]
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, "author", None) is None:
+            obj.author = request.user
+        obj.save()
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "content",
-        "parent_id",
+        "parent",
         "author",
         "theme",
         "created_at",
         "is_active",
     )
+    list_display_links = (
+        "id",
+        "content",
+    )
+    fields = [
+        "theme",
+        "content",
+        "parent",
+        "is_active",
+    ]
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, "author", None) is None:
+            obj.author = request.user
+        obj.save()
