58939b6ab2a6c3e5a762afaf97d7b02e262034b4
VadimBarinov <barinovvadim87@gmail.com>
2025-11-22 09:28:26 +0400

    final docker-compose

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = settings_app.app.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [settings_app.app.allowed_hosts]
 
