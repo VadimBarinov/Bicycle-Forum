3f39ad609126e3c416d52f25a3e6cf7a230aa6e8
VadimBarinov <barinovvadim87@gmail.com>
2025-12-17 00:58:46 +0400

    fixed message parent link

--- app/forum/static/forum/js/message_target.js
+++ app/forum/static/forum/js/message_target.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     setTimeout(function(){
       $('.message-on-theme').addClass('hide-before');
     }, 1000);
+    setTimeout(function(){
+      $('.message-on-theme').addClass('delete-before');
+    }, 3000);
 })
